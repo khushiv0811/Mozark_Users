ba9120d22aeba2f006213962cf6a0af90162a2ce
khushboo <khushiv30@gmail.com>
2021-03-12 16:37:41 +0530

    Initial Commit

---
+++ Mozark_Users/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mozark_Users
+//
+//  Created by khushboo patel on 12/03/21.
+//  Copyright © 2021 khushboo patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
